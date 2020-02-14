48460139b3eb432a8d389b907f73e659e13adf75
kabouama <karimbouamama7@gmail.com>
2020-02-14 20:13:55 +0100

    visu React -> cd visu -> npm start

---
+++ visu/src/Components/js/Body.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../css/body.css'
+import GradientButton from 'react-linear-gradient-button';
+
+class Body extends Component {
+
+  render() {
+    return (
+        <div className="body , center">
+            <div className="visu">
+                ok
+            </div>
+            <div className="visuInfo">
+                wee
+            </div>
+      </div>
+    );
+  }
+
+}
+
+export default Body;
